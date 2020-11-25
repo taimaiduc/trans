71c2696037019af7d106ef6fbad23c145bdf0fd0
mt <you@example.com>
2020-11-25 18:13:15 +0700

    feat(trans): commit

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$text = $_GET['s'] ?? 'Basically, we will change new flow for register. I just extent and create new file. Some UI will change in staging';
+$text = $_GET['s'] ?? die();
 
 echo "EN: \n";
 echo \Stichoza\GoogleTranslate\GoogleTranslate::trans($text, 'en', 'auto');
