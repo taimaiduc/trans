2eabd69f43edef2eef8d98e45a044889c6bd9880
mt <you@example.com>
2020-11-25 17:53:45 +0700

    feat(trans): commit

--- api/index.php
+++ api/index.php
@@ -2,21 +2,15 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $text = $_GET['s'] ?? 'Basically, we will change new flow for register. I just extent and create new file. Some UI will change in staging';
-$json = json_decode(file_get_contents('http://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=' . urlencode($text)));
-$json2 = json_decode(file_get_contents('http://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=vi&dt=t&q=' . urlencode($text)));
-$json3 = json_decode(file_get_contents('http://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=de&dt=t&q=' . urlencode($text)));
-$json4 = json_decode(file_get_contents('http://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=fr&dt=t&q=' . urlencode($text)));
-
 
 echo "EN: \n";
-
 echo \Stichoza\GoogleTranslate\GoogleTranslate::trans($text, 'en', 'auto');
 
 echo "\n VI:\n";
-
 echo \Stichoza\GoogleTranslate\GoogleTranslate::trans($text, 'vi', 'auto');
-echo "\n DE:\n";
 
+echo "\n DE:\n";
 echo \Stichoza\GoogleTranslate\GoogleTranslate::trans($text, 'de', 'auto');
+
 echo "\n FR:\n";
 echo \Stichoza\GoogleTranslate\GoogleTranslate::trans($text, 'fr', 'auto');
