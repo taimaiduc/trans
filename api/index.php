fd702c11c63bcf9e88b78a4ac0ae96fa0ea7c6fa
taimai <taimaiduc@gmail.com>
2021-05-21 11:22:18 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -6,21 +6,19 @@ $ios = $_GET['ios'] ?? false;
 header('Content-Type: text/plain; charset=utf-8');
 echo "EN: \n";
 echo \Stichoza\GoogleTranslate\GoogleTranslate::trans($text, 'en', 'auto');
+
 echo "\n";
-//usleep(50);
 echo "\nVI:\n";
 echo \Stichoza\GoogleTranslate\GoogleTranslate::trans($text, 'vi', 'auto');
-if($ios){
-exit(200);
-}
+
 echo "\n";
-//usleep(50);
 echo "\nDE:\n";
 echo \Stichoza\GoogleTranslate\GoogleTranslate::trans($text, 'de', 'auto');
+
 echo "\n";
-//usleep(50);
 echo "\nFR:\n";
 echo \Stichoza\GoogleTranslate\GoogleTranslate::trans($text, 'fr', 'auto');
+
 echo "\n";
 echo "\nPT:\n";
 echo \Stichoza\GoogleTranslate\GoogleTranslate::trans($text, 'pt', 'auto');
